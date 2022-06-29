b4480f0e30407534e045ec6c5de0ad0c4fcf5110
raashika <edularaashika@gmail.com>
2022-06-29 10:55:38 +0900

    update geoAnalytics

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name = 'geoAnalytics',
-    version = '2022.06.22.3',
+    version = '2022.06.30',
     author = 'Rage Uday Kiran',
     author_email = 'uday.rage@gmail.com',
     description = 'This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
