89990c5ca800048d07b432d76c2a7174654230d7
Raashika214 <edularaashika@gmail.com>
2023-07-21 12:51:57 +0900

    documentation of analytics and added raster2csv code

--- geoAnalytics/analytics.py
+++ geoAnalytics/analytics.py
@@ -1,7 +1,19 @@
 import pandas as pd
 import numpy as np
 
+
 def cosineSimilarity(tableName1, tableName2):
+    """
+    :Description: This function calculates the cosine similarity between two dataframes using the provided formula.
+    Cosine Similarity = Dot product of df1 and df2 / (Magnitude of df1 * Magnitude of df2)
+
+    :param tableName1: The first data frame containing 'x' and 'y' columns (point co-ordinates)
+    :param tableName2: The second data frame containing 'x' and 'y' columns (point co-ordinates)
+
+    :return: A new DataFrame with 'x' , 'y' and 'cosine similarity' columns
+    :rtype: DataFrame
+    """
+
     df1 = tableName1
     df2 = tableName2
 
@@ -23,14 +35,12 @@ def cosineSimilarity(tableName1, tableName2):
 
     # for each row in df2
     for i in range(len(df2)):
-        similarity_scores = df1.dot(df2)/ (np.linalg.norm(df1, axis=1) * np.linalg.norm(df2))
+        similarity_scores = df1.dot(df2) / (np.linalg.norm(df1, axis=1) * np.linalg.norm(df2))
         # get average of similarity scores
         average_similarity_score = np.average(similarity_scores)
         # append to dataframe
-        cosineSimilarityDF = cosineSimilarityDF.append({'X': x[i], 'Y': y[i], 'Cosine Similarity': average_similarity_score}, ignore_index=True)
-    
+        cosineSimilarityDF = cosineSimilarityDF.append(
+            {'X': x[i], 'Y': y[i], 'Cosine Similarity': average_similarity_score}, ignore_index=True)
+
     return cosineSimilarityDF
-    
-
-
 
