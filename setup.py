4312475c87b874f296f2d98985d622d60962e6ff
RAGE UDAY KIRAN <52146396+udayRage@users.noreply.github.com>
2023-11-19 00:45:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,8 @@ setuptools.setup(
         'resource',
         'validators',
         'urllib3',
-        'psycopg2-binary'
+        'psycopg2-binary',
+	'shapely',
     ],
     classifiers = [
         'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
