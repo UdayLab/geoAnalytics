cc0c52a5707ea90b26cdc3eeb31e9a8a4c62a87f
chara <charantejacherry2003@gmail.com>
2025-06-04 18:11:43 +0900

    #FuzzycMeans clustering

--- geoAnalytics/clustering/FuzzyCMeans.py
+++ geoAnalytics/clustering/FuzzyCMeans.py
@@ -1,3 +1,25 @@
+# Fuzzy C-Means (FCM) is a soft clustering algorithm where each data point can belong to multiple clusters with varying degrees of membership.
+#
+# **Importing this algorithm into a Python program**
+#
+#             from geoAnalytics.clustering import FuzzyCMeans
+#
+#             import pandas as pd
+#
+#             df = pd.read_csv("data.csv")
+#
+#             obj = FuzzyCMeans(df)
+#
+#             output = obj.clustering(n_clusters=3)
+#
+#             labels = output[0]
+#
+#             fcm.centers = output[1]
+#
+#             obj.save('FuzzyCMeansLabels.csv')
+#
+
+
 __copyright__ = """
 Copyright (C)  2022 Rage Uday Kiran
 
@@ -25,12 +47,64 @@ import pandas as pd
 
 
 class FuzzyCMeans:
+    """
+    **About this algorithm**
+
+    :**Description**: Fuzzy C-Means (FCM) is a soft clustering technique where each data point is assigned a membership score to all clusters,
+                      allowing partial membership instead of a hard assignment to a single cluster.
+
+    :**Parameters**: - **dataframe** (*pd.DataFrame*) -- * where:
+        - The first two columns are coordinates.
+        - The rest of the columns contain the features used for clustering.*
+
+    :**Attributes**: - **df** (*pd.DataFrame*) -- *A copy of your original data, with the first two columns renamed to `'x'` and `'y'` to make things easy and clear during clustering*
+
+
+    **Execution methods**
+
+    **Calling from a Python program**
+
+    .. code-block:: python
+
+            from geoAnalytics.clustering import FuzzyCMeans
+
+            import pandas as pd
+
+            df = pd.read_csv("data.csv")
+
+            obj = FuzzyCMeans(df)
+
+            output = obj.clustering(n_clusters=3)
+
+            labels = output[0]
+
+            fcm.centers = output[1]
+
+            obj.save('FuzzyCMeansLabels.csv')
+
+
+     **Credits**
+
+     The complete program was written by M. Charan Teja under the supervision of Professor Rage Uday Kiran.
+
+     """
+
     def __init__(self, dataframe):
+        """
+        Constructor that initializes the FuzzyCMeans object with input DataFrame.
+
+        :param dataframe:  - A Pandas DataFrame that contains the data to be clustered.
+        """
         self.df = dataframe.copy()
         self.df.columns = ['x', 'y'] + list(self.df.columns[2:])
         self.labelsDF = None
 
     def getStatistics(self, start_time):
+        """
+        Prints memory usage and runtime of the clustering function.
+
+        :param start_time: Time when the clustering function started execution.
+        """
         print("Total Execution time of proposed Algorithm:", time.time() - start_time)
         process = psutil.Process()
         memory_uss_kb = process.memory_full_info().uss / 1024
@@ -39,6 +113,15 @@ class FuzzyCMeans:
         print("Memory (RSS) of proposed Algorithm in KB:", memory_rss_kb)
 
     def clustering(self, n_clusters=3):
+        """
+        Performs fuzzy clustering on data.
+        -It excludes the 'x' and 'y' columns during clustering, and uses the remaining columns.
+        -After clustering, it adds the cluster label (based on the highest membership score) back to the original coordinates
+        :param n_clusters: Number of clusters to form (default is 3).
+        :return: A tuple containing:
+                 - A DataFrame with 'x', 'y', and cluster labels for each data point (based on maximum membership).
+                 - A NumPy array with coordinates of the cluster centers.
+        """
         start_time = time.time()
         data = self.df.drop(['x', 'y'], axis=1)
         data = data.to_numpy()
@@ -51,6 +134,9 @@ class FuzzyCMeans:
         return self.labelsDF, fcm.centers
 
     def save(self, outputFile='FuzzyCMeansLabels.csv'):
+        """
+        Save the outputFile in CSV
+        """
         if self.labelsDF is not None:
             try:
                 self.labelsDF.to_csv(outputFile, index=False)
