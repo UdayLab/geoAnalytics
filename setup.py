81d8bec05f9dbde563cb0dbfdff91b4b9b8f7d76
vanithakattumuri <vanithakattumuri32@gmail.com>
2025-06-04 22:27:04 +0900

    #updated pypi with version==2025.6.4.6

--- setup.py
+++ setup.py
@@ -1,37 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open('README.md', 'r') as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='geoanalytics',
-    version='2025.06.04.3',
-    author='Rage Uday Kiran',
-    author_email='uday.rage@gmail.com',
-    description='This software is being developed at the University of Aizu, Aizu-Wakamatsu, Fukushima, Japan',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    packages=setuptools.find_packages(),
-    url='https://github.com/udayRage/geoAnalytics',
-    license='GPLv3',
-    install_requires=[            # All necessary packages utilized by our PAMI software
-        'psutil',
-        'pandas',
-        'matplotlib',
-        'resource',
-        'validators',
-        'urllib3',
-        'psycopg2-binary',
-        'shapely',
-        'tqdm',
-        'mplcursors',
-        'sklearn'
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-        'Programming Language :: Python :: 3',
-	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.5',
-)
+setup()
--- uploadPYPI.sh
+++ uploadPYPI.sh
@@ -1,25 +1,29 @@
-echo "Uninstall old geoAnalytics version"
+echo "Uninstalling old geoAnalytics version"
 pip uninstall -y geoanalytics
 rm -rf dist/ geoanalytics.egg-info/ build/
 
+echo "Installing build and twine if not already present"
+python3 -m pip install --upgrade build twine
 
-echo "Running setup"
-python3 setup.py sdist bdist_wheel
+echo "Building source and wheel distributions"
+python3 -m build
 
-echo "Uploading to test repository"
-python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+echo "Uploading to TestPyPI"
+python3 -m twine upload --repository testpypi dist/*
 
-echo "Wait for 5 minute to update the repository"
-sleep 100
+echo "Waiting for TestPyPI to be ready"
+RETRIES=5
+for i in $(seq 1 $RETRIES); do
+    echo "Attempt $i: Installing from TestPyPI..."
+    python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps geoanalytics && break
+    sleep 10
+done
 
-echo "installing geoAnalytics from the testPYPI"
-python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps geoanalytics
-
-echo "Uploading geoAnalytics  to main PYPI repository"
+echo "Uploading to main PyPI"
 python3 -m twine upload dist/*
 
-echo "Deleting unnecessary files"
+echo "Cleaning up"
 rm -rf dist/ geoanalytics.egg-info/ build/
 
-
 echo "Completed."
+
