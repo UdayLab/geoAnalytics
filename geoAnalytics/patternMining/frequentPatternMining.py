8ac14a817086086a32c0a2df51f1b2b52468e5d8
udayRage <uday.rage@gmail.com>
2025-06-06 16:12:59 +0900

    adding large datasets into github

---
+++ geoAnalytics/patternMining/frequentPatternMining.py
@@ -0,0 +1,22 @@
+
+
+#Extract Data
+#data = self.df.drop(['x', 'y'], axis=1)
+
+# Concatenate X and Y columns of the dataframe using a tab space
+#newDF['new_col'] = 'POINT(' + df['col1'].astype(str) + ',' + df['col2'].astype(str) + ')'
+
+# Join new dataframe with the data dataframe
+#newDF = pd.concat([newDF, data.reset_index(drop=True)], axis=1)
+
+# Transporse the dataframe
+#denseDF = newDF.T
+
+# Convert dense dataframe into various formats  using conversion.DF2DB.py (Parameters: threshold value and condition)
+
+
+# Implement the frequent pattern mining algorithm.
+
+# Discover the patterns
+
+# Print the results.
