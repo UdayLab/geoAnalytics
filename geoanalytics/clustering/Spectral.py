5ad5936894d7c76ef4f5b217adbc61b37e3087ca
chara <charantejacherry2003@gmail.com>
2025-06-10 12:09:41 +0900

    # variable change

--- geoanalytics/clustering/Spectral.py
+++ geoanalytics/clustering/Spectral.py
@@ -1,3 +1,22 @@
+# Spectral Clustering performs graph-based partitioning using eigen decomposition of similarity matrices. It is especially suitable for identifying clusters that are not necessarily spherical or linearly separable.
+#
+# **Importing and Using Spectral Clustering in a Python Program**
+#
+#         import pandas as pd
+#
+#         from goeAnalytics.clustering import Spectral
+#
+#         df = pd.read_csv('data.csv')
+#
+#         obj = Spectral(df)
+#
+#         obj.clustering(n_clusters=4)
+#
+#         labels_df = output[0]
+#
+#         obj.save(outputFile='SpectralLabels.csv')
+#
+
 __copyright__ = """
 Copyright (C)  2022 Rage Uday Kiran
 
@@ -25,7 +44,47 @@ import pandas as pd
 
 
 class Spectral:
+    """
+    **About this algorithm**
+
+    :**Description**: Spectral Clustering uses graph-based methods to cluster points based on the eigenvalues
+                      of a similarity matrix. It's particularly effective for complex cluster shapes that
+                      traditional methods like KMeans may not detect.
+
+    :**Parameters**:   - **dataframe** (*pd.DataFrame*) -- Input dataset where the first two columns are assumed to be spatial ('x', 'y') and all remaining columns are used for clustering.
+
+    :**Attributes**:   - **df** (*pd.DataFrame*) -- Cleaned and formatted input data.
+                       - **labelsDF** (*pd.DataFrame*) -- Output containing 'x', 'y', and assigned cluster labels.
+
+    **Execution methods**
+
+    .. code-block:: python
+
+            import pandas as pd
+
+            from goeAnalytics.clustering import Spectral
+
+            df = pd.read_csv("data.csv")
+
+            obj = Spectral(df)
+
+            obj.clustering(n_clusters=4)
+
+            labels_df = output[0]
+
+            obj.save(outputFile="SpectralLabels.csv")
+
+    **Credits**
+
+    This implementation was created and revised under the guidance of Professor Rage Uday Kiran.
+
+    """
     def __init__(self, dataframe):
+        """
+        Constructor to initialize the Spectral Clustering object with input DataFrame.
+
+        :param dataframe: A pandas DataFrame with at least two spatial columns and feature columns.
+        """
         self.df = dataframe.copy()
         self.df.columns = ['x', 'y'] + list(self.df.columns[2:])
         self.labelsDF = None
@@ -53,6 +112,13 @@ class Spectral:
         print("Memory (RSS) of proposed Algorithm in KB:", self.memoryRSS)
 
     def run(self, n_clusters=8, assign_labels='discretize'):
+        """
+        Applies Spectral Clustering to the dataset.
+
+        :param n_clusters: Number of clusters to form.
+        :param assign_labels: Method for assigning labels after clustering ('kmeans' or 'discretize').
+        :return: A DataFrame containing x, y, and assigned labels.
+        """
         self.startTime = time.time()
         data = np.ascontiguousarray(np.array(self.df.drop(['x', 'y'], axis=1)))
         spectralClustering = SpectralClustering(assign_labels=assign_labels, n_clusters=n_clusters, random_state=0).fit(data)
@@ -68,6 +134,11 @@ class Spectral:
         return self.labelsDF
 
     def save(self, outputFile='SpectralLabels.csv'):
+        """
+        Saves the clustering results to a CSV file.
+
+        :param outputFile: Filename to save the resulting DataFrame.
+        """
         if self.labelsDF is not None:
             try:
                 self.labelsDF.to_csv(outputFile, index=False)
